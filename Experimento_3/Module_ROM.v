722366f7ec68240cbdb3c5ed28f08f77591cbf3c
berni <berni.mora@hpe.com>
2016-05-04 10:11:01 -0600

    Cambio en la ROM, IMUL por SMUL. Eliminar modulos IMUL y MUXLut

--- Experimento_3/Module_ROM.v
+++ Experimento_3/Module_ROM.v
@@ -16,8 +16,8 @@ begin
         1: oInstruction = { `STO, `R7,16'b0001 };
         2: oInstruction = { `STO, `R3,16'h1     };
         3: oInstruction = { `STO, `R4,16'd1000  };
-		  4: oInstruction = { `STO, `R6,16'd2     };
-		  5: oInstruction = { `IMUL, `E0,`R3,`R7   };		  
+	4: oInstruction = { `STO, `R6,16'd2     };
+	5: oInstruction = { `SMUL, `E0,`R3,`R7   };		  
         6: oInstruction = { `STO, `R5,16'd0     }; 		  
 //LOOP2:
         7: oInstruction = { `LED ,8'b0,`E0,8'b0 };
@@ -30,7 +30,7 @@ begin
         12: oInstruction = { `ADD ,`R5,`R5,`R3    };
         13: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };
         14: oInstruction = { `NOP ,24'd4000       };
-        15: oInstruction = { `IMUL ,`E0,`E0,`R6    };
+        15: oInstruction = { `SMUL ,`E0,`E0,`R6    };
         16: oInstruction = { `JMP ,  8'd6,16'b0   };
         default:
                 oInstruction = { `LED ,  24'b10101010 };                //NOP
